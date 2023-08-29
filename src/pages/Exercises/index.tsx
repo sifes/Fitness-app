f8728c0d611800b1136d06fb1d4a9acaca6417d7
sifes <lonfark@gmail.com>
2023-08-29 23:30:03 +0300

    search (not all)

--- src/pages/Exercises/index.tsx
+++ src/pages/Exercises/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import Categories from '../../components/exercises/categories/Categories'
 import Search from '../../components/exercises/Search'
 import { IExercise } from '../../types'
 import ExerciseCard from '../../components/exercises/ExerciseCard'
+import BodyParts from '../../components/exercises/bodyParts/BodyParts'
+import { useGetExercisesQuery } from '../../store/api'
+import { useCustomDispatch, useCustomSelector } from '../../hooks'
+import { setExercises } from '../../store/slices/exersicesSlice'
 
 const Exercises: React.FC = () => {
+    const dispatch = useCustomDispatch()
+    const { selectedBodyPart, searchValue, exercises } = useCustomSelector(state => state.exercisesReducer)
+    const { data, error, isLoading } = useGetExercisesQuery(`bodyPart/${selectedBodyPart}`)  // 
+
+    function helper(arr: IExercise[], pages: number) {
+        const EXERCISES = [...arr]
+        if (searchValue) {
+            EXERCISES.filter((item) => item.bodyPart.includes(searchValue) || item.equipment.includes(searchValue) || item.name.includes(searchValue) || item.target.includes(searchValue))
+
+        }
+        return setExercises(EXERCISES.splice(0, 9))
+    }
+
+    React.useEffect(() => {
+        const ExercisesData = data ? [...data] : [] as IExercise[]
+        data ? dispatch(helper(ExercisesData, 1)) : null
+    }, [data])
+
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error occured</div>
+
+    console.log(exercises);
+
     const array: IExercise[] = [
         { bodyPart: "neck", equipment: "body weight", gifUrl: "https://api.exercisedb.io/image/QDlmaKiO8LQX4A", id: "0716", name: "side push neck stretch", target: "levator scapulae", },
         { bodyPart: "neck", equipment: "body weight", gifUrl: "https://api.exercisedb.io/image/RfHODIySokMbJK", id: "1403", name: "neck side stretch", target: "levator scapulae", },
@@ -16,9 +43,9 @@ const Exercises: React.FC = () => {
             <div className="exercises">
                 <h2 className="exercises__title">Get Awesome Exersices!</h2>
                 <Search />
-                <Categories />
+                <BodyParts />
                 <div className='exercises__cards' >
-                    {array.map(exerciseData => <ExerciseCard key={exerciseData.id} {...exerciseData} />)}
+                    {exercises.length ? exercises.map(exerciseData => <ExerciseCard key={exerciseData.id} {...exerciseData} />) : ''}
                 </div>
             </div>
         </div>
