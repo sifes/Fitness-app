a6b27aacd199d84b95a80133f7afbf098a105f84
sifes <lonfark@gmail.com>
2023-11-30 09:06:13 +0200

    styles: exercise page

--- src/pages/DetailedExercise.tsx
+++ src/pages/DetailedExercise.tsx
@@ -13,31 +13,35 @@ const DetailedExercise: React.FC<IDetailedExercise> = () => {
     if (isLoading || !data) return <div>Loading...</div>
     if (error) return <div>Error occured</div>
     return (
-        <div className='container'>
-            <div className='detailed-exercise'></div>
-            <NavLink to={PATH.EXERCISES} className='detailed-exercise__link'>Back</NavLink>
-            <div className='detailed-exercise__info exercise-detailed'>
-                <div className="exercise-detailed__desc">
-                    <h4>{data.name}</h4>
-                    <ul>
-                        <li>
-                            <div>body part: </div><span>{data.bodyPart}</span>
-                        </li>
-                        <li>
-                            <div>equipment: </div><span>{data.equipment}</span>
-                        </li>
-                        <li>
-                            <div>target: </div><span>{data.target}</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="exercise-detailed__image">
-                    <img src={data.gifUrl} alt={data.bodyPart} />
-                </div>
-            </div>
+      <div className='container'>
+        <div className='detailed-exercise'></div>
+        <NavLink to={PATH.EXERCISES} className='detailed-exercise__link'>
+          Back
+        </NavLink>
+        <div className='detailed-exercise__info exercise-detailed'>
+          <div className='exercise-detailed__desc'>
+            <h4>{data.name}</h4>
+            <ul>
+              <li>
+                <div>body part: </div>
+                <span>{data.bodyPart}</span>
+              </li>
+              <li>
+                <div>equipment: </div>
+                <span>{data.equipment}</span>
+              </li>
+              <li>
+                <div>target: </div>
+                <span>{data.target}</span>
+              </li>
+            </ul>
+          </div>
+          <div className='exercise-detailed__image'>
+            <img src={data.gifUrl} alt={data.bodyPart} />
+          </div>
         </div>
-
-    )
+      </div>
+    );
 }
 
 export { DetailedExercise }
